d43431f80ff05ba580325cc633007ea776444fa2
nevasys <n.vahram@gmail.com>
2017-02-19 12:53:41 -0500

    agencecontroller
    add function geolocation by distance

---
+++ OurProject/CarRental/CarRental/Common/Commun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRental.Common
+{
+    public enum UserRole
+    {
+        ADMIN = 1,
+        AGENCE = 2,
+        CLIENT = 3,
+    }
+
+    public static class Commun 
+    {
+        
+        public static string GetUserRole(UserRole role)
+        {
+            switch(role)
+            {
+                case UserRole.ADMIN:
+                    return "Admin";
+                case UserRole.AGENCE:
+                    return "Agency";
+                case UserRole.CLIENT:
+                    return "Client";
+                default:
+                    return "";
+            }
+ 
+        }
+    }
+}
